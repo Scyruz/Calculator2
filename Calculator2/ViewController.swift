9e8172920e958ebc202097ff666fe121a2e21c3c
Priscila Ruiz <A01561869@itesm.mx>
2021-03-04 11:55:21 -0800

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by user188339 on 3/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
